889ebf688fcaabd70f3b94bddd30244eb55c5dec
Ramon Carvalho <rcarvalho-pb@gmail.com>
2024-08-26 06:07:10 -0300

    feat: create broker-service

---
+++ broker-service/cmd/api/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+var webPort = "8080"
+
+type Config struct{}
+
+func main() {
+
+	app := Config{}
+
+	log.Printf("Starting server on port %s\n", webPort)
+
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", webPort),
+		Handler: app.routes(),
+	}
+
+	if err := srv.ListenAndServe(); err != nil {
+		log.Panic(err)
+	}
+}
